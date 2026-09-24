19a107ef3e35bc8748433aece33794dfa1837d16
Avery Felts <averyfelts@Averys-MacBook-Air.local>
2026-09-24 07:59:02 -0600

    chore: bump internal version to 0.73.0

--- app/src-c/include/metalsharp_backend/backend.h
+++ app/src-c/include/metalsharp_backend/backend.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#define MS_BACKEND_DEFAULT_VERSION "0.72.0"
+#define MS_BACKEND_DEFAULT_VERSION "0.73.0"
 #ifndef MS_BACKEND_VERSION
 #define MS_BACKEND_VERSION MS_BACKEND_DEFAULT_VERSION
 #endif
